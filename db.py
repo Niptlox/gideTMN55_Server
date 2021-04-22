2d7290a2919d7d08ef5e70ddc29cb5439753f4b6
Niptlox <GeorgeGoldobin@yandex.ru>
2021-04-22 17:19:33 +0500

    data 0

--- db.py
+++ db.py
@@ -119,7 +119,7 @@ def get_questtours(db_sess):
     tours = []
     for tour in db_sess.query(QuestTour).all():
         tours.append(tour.get_info())
-    return tours
+    return tours[0]
 
 
 def get_vidiotours(db_sess):
